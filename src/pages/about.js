7b2e08d98b79b6f421b13b049050b865469cb8ce
Bruno Monteiro <brunomon@gmail.com>
2021-01-15 13:24:44 +0000

    first commit

---
+++ src/pages/about.js
@@ -0,0 +1,10 @@
+import React from "react"
+import Header from "../components/header"
+
+export default function About() {
+  return <div style={{ color: `teal` }}>
+    <Header headerText="About Gatsby"/>
+    <Header headerText="It's pretty cool"/>
+    <a>Such wow.Very React.</a>
+  </div>
+}
